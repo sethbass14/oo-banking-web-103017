5a5a66a4c2c9e6fe27dc5b70c6462a210846b342
github username <github email address>
2017-11-02 15:26:03 -0400

    Priyam and Seth finished bank lab

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :amount
+  attr_reader :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    # Checks to see if both account are 'open', have money && have enough funds && the transaction is not complete
+    (@sender.valid? && @receiver.valid?) && @sender.balance >= amount && @status != "complete"
+  end
+
+  def execute_transaction
+    if self.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    end
+
+  end
+
 end
